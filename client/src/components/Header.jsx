3ffad304a9a2c8aadf80f92ec60d4a4fc37dcdb6
Jha-Shiva <shivamjha625@gmail.com>
2025-09-01 13:49:07 +0530

    text size change in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { Button, DarkThemeToggle, Navbar, NavbarCollapse, NavbarLink, NavbarTogg
 const Header = () => {
   const path = useLocation().pathname;
   return (
-    <Navbar fluid className="border-b-2">
+    <Navbar fluid className="shadow-xl">
       <Link
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-2xl font-semibold dark:text-white" id='blog-name'
@@ -30,6 +30,7 @@ const Header = () => {
         <AiOutlineSearch />
       </Button>
       <div className='flex md:order-2'>
+        
         <DarkThemeToggle className=' text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4
         focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 
         dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 sm:inline'/>
@@ -43,13 +44,13 @@ const Header = () => {
       </div>
         <NavbarCollapse>
           <NavbarLink as={'div'} active={path === '/'}>
-              <Link to='/'>Home</Link>
+              <Link to='/' className='md:text-lg text-sm'>Home</Link>
           </NavbarLink>
           <NavbarLink as={'div'} active={path === '/about'}>
-              <Link to='/about'>About</Link>
+              <Link to='/about' className='md:text-lg text-sm'>About</Link>
           </NavbarLink>
           <NavbarLink as={'div'} active={path === '/projects'}>
-              <Link to='/projects'>Projects</Link>
+              <Link to='/projects' className='md:text-lg text-sm'>Projects</Link>
           </NavbarLink>
         </NavbarCollapse>
     </Navbar>
