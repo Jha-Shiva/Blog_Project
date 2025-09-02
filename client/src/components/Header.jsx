b7a4ace2fff9b4c57920ca91678e53fc51161301
Jha-Shiva <shivamjha625@gmail.com>
2025-09-02 16:55:57 +0530

    Created signup UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,22 +25,25 @@ const Header = () => {
         />
       </form>
       <Button className="lg:hidden text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4
-      focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white
+      focus:ring-gray-100 font-medium rounded-full text-sm me-2 mb-2 dark:bg-gray-800 dark:text-white
       dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" >
         <AiOutlineSearch />
       </Button>
-      <div className='flex md:order-2'>
+
+      <div className='flex md:order-2 gap-2'>
         
-        <DarkThemeToggle className=' text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4
+        <DarkThemeToggle className='hidden text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4
         focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 
         dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 sm:inline'/>
 
         <Link to='/sign-in'>
-          <Button pill>
-            Sign In
-          </Button>
+          <button class=" relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-3xl group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-3xl group-hover:bg-transparent group-hover:dark:bg-transparent">
+              Sign In
+            </span>
+          </button>
         </Link>
-        <NavbarToggle/>
+        <NavbarToggle className='w-10 h-10 shadow-sm'/>
       </div>
         <NavbarCollapse>
           <NavbarLink as={'div'} active={path === '/'}>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,66 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1 '>
+          <Link
+          to="/"
+          className="font-bold dark:text-white text-4xl" id='blog-name'
+          >
+            <span className="px-3 py-1 bg-gradient-to-r from-red-500 via-amber-600 border-t-rose-700 rounded-lg text-white/70 hover:text-white ">
+              Echo
+            </span>{" "}
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a blog project. You can sign in with your email and password or with Google.
+          </p>
+        </div>
+        
+        {/* right */}
+        <div className='flex-1 shadow-xl p-6 rounded-xl'>
+          <form action="" className='flex flex-col gap-4'>
+            <div>
+              <Label htmlFor='username'>Your username</Label>
+              <TextInput id='username' type='text' name='username' placeholder='username' required/>
+            </div>
+
+            <div>
+              <Label htmlFor='email'>Your email</Label>
+              <TextInput id='email' type='email' name='email' placeholder='example@gmail.com' required/>
+            </div>
+            
+            <div>
+              <Label htmlFor='password'>Your password</Label>
+              <TextInput id='password' type='password' name='password' placeholder='password' required/>
+            </div>
+
+            <Button
+              className='bg-gradient-to-r from-purple-500 to-pink-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800'
+              type='submit'
+            >
+              Sign Up
+            </Button>
+
+          </form>
+
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account ?</span>
+            <Link to={'/sign-in'}
+              className='text-blue-600'
+            >
+              Sign In
+            </Link>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
