db6f3ac3acb55a8a211eea1e057e240c82e0ef9b
Jha-Shiva <shivamjha625@gmail.com>
2025-09-04 18:07:02 +0530

    Profile page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,9 +1,47 @@
+import { TextInput } from 'flowbite-react'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const DashProfile = () => {
+  const {currentUser} = useSelector((state)=> state.user)
   return (
-    <div>DashProfile</div>
-  )
+    <div className="max-w-lg mx-auto p-4 w-full">
+      <h1 className="my-7 text-center font-semibold roboto text-3xl">
+        Profile
+      </h1>
+      <form className="flex flex-col gap-4 mt-4">
+        <div className="w-34 h-32 self-center cursor-pointer shadow-md shadow-gray-400 rounded-full overflow-hidden">
+          <img
+            src={currentUser.rest.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.rest.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.rest.email}
+        />
+        <TextInput type="password" id="password" placeholder="password" />
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span class="relative w-full px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+            Update
+          </span>
+        </button>
+      </form>
+      <div className="text-red-500 mt-5 flex justify-between">
+        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
+  );
 }
 
 export default DashProfile
