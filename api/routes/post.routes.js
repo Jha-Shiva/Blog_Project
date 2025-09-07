ad168c5b4fecec3b915643bbbcd31ae19fbe7033
Jha-Shiva <shivamjha625@gmail.com>
2025-09-07 23:21:46 +0530

    Created api routes for getting all posts

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getAllPosts } from '../controllers/post.controller.js';
 import { verifyToken } from '../middlewares/verifyUser.js';
 
 const router = express.Router();
 
 //create a new post
 router.post('/create', verifyToken, createPost)
+router.get('/getposts', getAllPosts)
 
 
 export default router;
