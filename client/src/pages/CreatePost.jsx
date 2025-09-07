a8135c5fa19ac5f19289cee40692bf4d5cb37b61
Jha-Shiva <shivamjha625@gmail.com>
2025-09-07 20:20:37 +0530

    Completed createPost page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,15 +3,17 @@ import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import React, { useMemo, useRef, useState } from 'react';
 import JoditEditor from 'jodit-react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 const CreatePost = () => {
     const editor = useRef(null);
     const [content, setContent] = useState('');
     const isDarkMode = useSelector((state) => state.theme.theme === 'dark' );
-    console.log(isDarkMode);
-    console.log(content);
-    
+    const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
+
     const config = useMemo(()=>(
         {
       readonly: false,
@@ -43,12 +45,38 @@ const CreatePost = () => {
       ],
     }
     ),[isDarkMode])
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  };
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-3xl text-center my-7 font-semibold roboto">
         Create a Post
       </h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
             type="text"
@@ -57,8 +85,10 @@ const CreatePost = () => {
             id="title"
             name="title"
             className="flex-1"
+            onChange={(e)=>setFormData({...formData, title: e.target.value})}
           />
-          <Select id="category" name="category" required className="flex-1">
+          <Select onChange={(e)=> setFormData({...formData, category: e.target.value})}
+           id="category" name="category" required className="flex-1">
             <option value="">Select Category</option>
             <option value={"javascript"}>Javascript</option>
             <option value={"reactjs"}>React.js</option>
@@ -66,7 +96,7 @@ const CreatePost = () => {
             <option value={"mongodb"}>Mongo Db</option>
           </Select>
         </div>
-        <div
+        {/* <div
           className="flex gap-4 items-center justify-between border-4
             border-blue-500 border-dotted p-3"
         >
@@ -79,29 +109,29 @@ const CreatePost = () => {
           <Alert color="info" className="!mb-0 !rounded-md !p-2 !text-sm">
             Image size should be less than 1MB
           </Alert>
-        </div>
-        <div className="flex gap-4 items-center justify-between p-3">
+        </div> */}
+        <div className="border-4
+            border-blue-500 border-dotted p-3">
           <TextInput
             type="url"
             placeholder="link of the image"
-            id="imagelink"
-            name="imagelink"
+            id="image"
+            name="image"
             className="flex-1"
+            onChange={(e)=> setFormData({...formData, image: e.target.value})}
           />
-          <Button color="blue" className="whitespace-nowrap">
-            Upload Link
-          </Button>
         </div>
         <div className="mt-4 ">
           <JoditEditor
             config={config}
             ref={editor}
             value={content}
-            onChange={setContent}
+            // onChange={setContent}
+            onChange={(value) => setFormData({...formData, content: value})}
           />
           <div className="mt-4 border p-2 bg-gray-100 dark:bg-gray-800">
             <strong>Preview:</strong>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
+            <div dangerouslySetInnerHTML={{ __html: formData.content }} />
           </div>
           <button className="mt-4 w-full relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
             <span className="relative w-full px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
@@ -109,6 +139,13 @@ const CreatePost = () => {
             </span>
           </button>
         </div>
+        {
+          publishError && (
+            <Alert color='failure' className='!mb-0 !rounded-md !p-2 !text-sm'>
+              {publishError}
+            </Alert>
+          )
+        }
       </form>
     </div>
   );
