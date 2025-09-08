7b2e218a682b707abfa8e4c47c51666c3fc81105
Jha-Shiva <shivamjha625@gmail.com>
2025-09-08 11:57:32 +0530

    Show user posts inside dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useSelector } from 'react-redux'
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
+import { Link } from 'react-router-dom';
 
 const DashPosts = () => {
+  const {currentUser} = useSelector((state)=>state.user);
+  const [userPosts, setUserPosts] = useState([])
+  console.log(userPosts[0]?.updatedAt);
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser.rest._id}`);
+        const data = await res.json();
+        if(res.ok){
+          setUserPosts(data.posts)
+
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    if(currentUser.rest.isAdmin){
+      fetchPosts()
+    }
+  },[currentUser.rest._id])
   return (
-    <div>DashPosts</div>
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.rest.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <TableHead>
+              <TableRow>
+                <TableHeadCell>Date Updated</TableHeadCell>
+                <TableHeadCell>Post Image</TableHeadCell>
+                <TableHeadCell>Post Title</TableHeadCell>
+                <TableHeadCell>Category</TableHeadCell>
+                <TableHeadCell>Delete</TableHeadCell>
+                <TableHeadCell>
+                  <span>Edit</span>
+                </TableHeadCell>
+              </TableRow>
+            </TableHead>
+            
+            <TableBody className='divide-y'>
+              {userPosts.map((post)=>(
+                  <TableRow key={post._id} className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <TableCell>{new Date(post.updatedAt).toLocaleDateString()}</TableCell>
+                    <TableCell>
+                      <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className='w-20 h-10 object-cover bg-gray-500'
+                      />
+                    </Link>
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                    </TableCell>
+                    <TableCell>{post.category}</TableCell>
+                    <TableCell>
+                      <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                      >
+                      Delete
+                      </span>
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-post/${post._id}`}
+                      >
+                        <span>Edit</span>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </>
+      ) : (
+        <p>You have no posts yet</p>
+      )}
+    </div>
   )
 }
 
